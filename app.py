45807577506001a221c1367c25a4a4c33de3d3b2
beardednotary <thebeardednotary@gmail.com>
2026-02-16 11:43:26 -0800

    Updated flask CORS in app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 import stripe
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=['https://checkbiolink.com', 'https://app.checkbiolink.com'])
+app.config.from_object(Config)
 
 # Load environment variables from .env file
 load_dotenv()
